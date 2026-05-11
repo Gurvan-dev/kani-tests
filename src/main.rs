7bef18f1e14f6fbcf49b826422509281f0a230c6
Gurvan Debaussart <gurvan@debauss.art>
2026-05-11 15:18:52 +0200

    feat: first verification ok

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-#[cfg(any(test, feature = "userspace"))]
+#[cfg(any(test, feature = "userspace", kani))]
 use softcore_rv64::{Core, config, new_core};
 
-// Each thread gets its own copy of the core, this prevent tests using different threads inside a
-// same process to share the same core.
-#[cfg(any(test, feature = "userspace"))]
+// Macros and utils---------------------------------------------------------------------------------
+
+#[cfg(any(test, feature = "userspace", kani))]
 std::thread_local! {
 
     pub static SOFT_CORE: core::cell::RefCell<Core> = {
@@ -13,17 +13,16 @@ std::thread_local! {
     };
 }
 
-/// Unsafe placeholder function to make softcore-asm unsafe.
 unsafe fn _unsafe_marker() {}
 
 macro_rules! soft_asm {
     ($($asm:tt)*) => {{
-        #[cfg(not(any(test, feature = "userspace")))]
+        #[cfg(not(any(test, feature = "userspace", kani)))]
         core::arch::asm!(
             $($asm)*
         );
 
-        #[cfg(any(test, feature = "userspace"))]
+        #[cfg(any(test, feature = "userspace", kani))]
         softcore_asm_rv64::asm!(
             $($asm)*,
             softcore(SOFT_CORE.with_borrow_mut)
@@ -31,49 +30,43 @@ macro_rules! soft_asm {
             // softcore_trap_handlers(_tracing_trap_handler, _mprv_trap_handler, _raw_trap_handler)
         );
 
-        #[cfg(any(test, feature = "userspace"))]
+        #[cfg(any(test, feature = "userspace", kani))]
         _unsafe_marker();
     }};
 }
 
-pub fn vec_add<const LEN: usize>(inp1: &[u8; LEN], inp2: &[u8; LEN], out: &mut[u8; LEN]) -> usize {
-    let vl: usize;
+// Functions to verify -----------------------------------------------------------------------------
 
-    // TODO: We need to mark in the signature that we use v1, v3, v2…
+pub fn vsetvli(len: usize) -> usize {
+    let vl: usize;
     unsafe {
         soft_asm!(
             "vsetvli {vl}, {len}, e8, m1, ta, ma",
-            "vle8.v v1, ({ptr_a})",
-            "vle8.v v2, ({ptr_b})",
-            "vadd.vv v3, v1, v2",
-            "vse8.v v3, ({ptr_out})",
-
             vl = out(reg) vl,
-            len = in(reg) LEN,
-            ptr_a = in(reg) inp1.as_ptr(),
-            ptr_b = in(reg) inp2.as_ptr(),
-            ptr_out = in(reg) out.as_mut_ptr(),
-        );
+            len = in(reg) len,
+        )
     }
-
     return vl;
 }
 
-pub fn vec_add_second(inp1: &[u8], inp2: &mut [u8], len: usize) -> usize {
+/// add `inp1` to `inp2` in place
+/// Inspired by `ffmpeg` function `ff_llvid_add_bytes_rvv`
+/// TODO: We could try to do it in-place first?
+pub fn vec_add_second_partial(inp1: &[u8], inp2: &mut [u8], len: usize) -> usize {
     let vl: usize;
 
-    assert!(inp1.len() < len);
-    assert!(inp2.len() < len);
+    assert!(len < inp1.len());
+    assert!(len < inp2.len());
 
-    // TODO: We need to mark in the signature that we use v1 and v2
+    // TODO: We need to mark in the signature that we use vector registers
     // TODO: Try to increase from m1 -> m8
     unsafe {
         soft_asm!(
             "vsetvli {vl}, {len}, e8, m1, ta, ma",
-            "vle8.v v1, ({ptr1})",
-            "vle8.v v2, ({ptr2})",
-            "vadd.vv v2, v1, v2",
-            "vse8.v v2, ({ptr2})",
+            "vle8.v v0, ({ptr1})",
+            "vle8.v v8, ({ptr2})",
+            "vadd.vv v8, v0, v8",
+            "vse8.v v8, ({ptr2})",
 
             vl = out(reg) vl,
             len = in(reg) len,
@@ -82,52 +75,55 @@ pub fn vec_add_second(inp1: &[u8], inp2: &mut [u8], len: usize) -> usize {
         );
     }
 
+    assert!(len == 0 || 0 < vl);
+
     return vl;
 }
 
-// fn add_bytes_using_vec_add(inp1: &[u8], inp2: &mut [u8], mut len: usize) {
-//     let mut vl: usize;
-//     while 0 < len {
-//         /* TODO: We need to increase inp1 and inp2 by vl here */
-//         vl = vec_add_second(inp1, inp2, len);
-//         assert!(0 < vl);
-//         len -= vl;
-//     }
-// }
+/* TODO: Might be easier to do this not in place */
+fn vec_add_second(mut inp1: &[u8], mut inp2: &mut [u8], mut len: usize) {
+    let mut vl: usize;
+    while 0 < len {
+        // TODO: We need to increase inp1 and inp2 by vl here
+        vl = vec_add_second_partial(inp1, inp2, len);
+        /* TODO (Loop invariant): Say that we have the addition we want */
+        assert!(0 < vl);
+        inp1 = &inp1[vl..];
+        inp2 = &mut inp2[vl..];
+        len -= vl;
+    }
 
-/// TODO
-/// Add vectorx x and y, with y in-place
-/// From `ffmpeg` function `ff_llvid_add_bytes_rvv`
-// fn add_bytes(x: &[u8], y: &mut [u8], mut w: usize) {
-//     while 0 < w {
-//         // We could have a version relying on vec_add to work
-//         unsafe {
-//             soft_asm!(
-//                 "vsetvli t0, {w}, e8, m8, ta, ma",
-//                 "vle8.v  v0, (a1)",
-//                 "sub     a2, {w}, t0",
-//                 "vle8.v  v8, (a0)",
-//                 "add     a1, t0, a1",
-//                 "vadd.vv v8, v0, v8",
-//                 "vse8.v  v8, (a0)",
-//                 "add     a0, t0, a0",
-//
-//                 w = inout(reg) w,
-//             );
-//         }
-//     }
-// }
+    assert!(len == 0);
+}
+
+
+// Proofs ------------------------------------------------------------------------------------------
 
 #[cfg(kani)]
 #[kani::proof]
-fn check_vec_add_ok() {
+fn check_vsetvli_non_zero() {
+    let len: usize = 4;
+    let vl = vsetvli(len);
+    assert!(len == 0 || 0 < vl);
+}
+
+#[cfg(kani)]
+#[kani::proof]
+fn check_vec_add_second_partial() {
     const LEN: usize = 4;
+    const LEN_COPY: usize = 4;
     let inp1: [u8; LEN] = kani::any();
-    let inp2: [u8; LEN] = kani::any();
-    let mut out: [u8; LEN] = kani::any();
-    let vl = vec_add(&inp1, &inp2, &mut out);
+    let mut inp2: [u8; LEN] = kani::any();
+    let old_inp2 = inp2.clone();
+    for i in 0..LEN {
+        assert_eq!(old_inp2[i], inp2[i]);
+    }
+    kani::assume(LEN_COPY < LEN);
+    kani::assume(LEN == inp1.len());
+    kani::assume(LEN == inp2.len());
+    let vl = vec_add_second_partial(&inp1, &mut inp2, LEN_COPY);
     for i in 0..vl {
-        assert_eq!(out[i], inp1[i] + inp2[i]);
+        assert_eq!(inp2[i], inp1[i] + old_inp2[i]);
     }
 }
 
@@ -135,55 +131,20 @@ fn check_vec_add_ok() {
 #[kani::proof]
 fn check_vec_add_second() {
     const LEN: usize = 4;
+    const LEN_COPY: usize = 4;
     let inp1: [u8; LEN] = kani::any();
     let mut inp2: [u8; LEN] = kani::any();
     let old_inp2 = inp2.clone();
     for i in 0..LEN {
         assert_eq!(old_inp2[i], inp2[i]);
     }
-    let len: usize = kani::any();
-    kani::assume(len < LEN);
+    kani::assume(LEN_COPY < LEN);
     kani::assume(LEN == inp1.len());
     kani::assume(LEN == inp2.len());
-    let vl = vec_add_second(&inp1, &mut inp2, len);
-    for i in 0..vl {
+    vec_add_second(&inp1, &mut inp2, LEN_COPY);
+    for i in 0..LEN_COPY {
         assert_eq!(inp2[i], inp1[i] + old_inp2[i]);
     }
 }
 
-// #[cfg(kani)]
-// #[kani::proof]
-// fn check_add_bytes_using_vec_add_ok() {
-//     const LEN: usize = 4;
-//     let inp1: [u8; LEN] = kani::any();
-//     let inp2: [u8; LEN] = kani::any();
-//     let old_inp2 = inp2.clone();
-//     for i in 0..LEN {
-//         assert_eq!(old_inp2[i], inp2[i]);
-//     }
-//     let len: usize = kani::any();
-//     kani::assume(len < LEN);
-//     kani::assume(inp1.len() == i);
-//     let vl = add_bytes_using_vec_add(&inp1, &mut inp2, len);
-//     /* TODO: Is the following true? How is overflow handled? trap? */
-//     // for i in 0..vl {
-//     //     assert_eq!(out[i], inp1[i] + inp2[i]);
-//     // }
-// }
-//
-// #[cfg(kani)]
-// #[kani::proof]
-// fn check_add_bytes_ok() {
-//     const LEN: usize = 4;
-//     let inp1: [u8; LEN] = kani::any();
-//     let inp2: [u8; LEN] = kani::any();
-//     let out: [u8; LEN] = kani::any();
-//     let vl = vec_add(inp1, inp2, out);
-//     /* TODO: Is the following true? How is overflow handled? trap? */
-//     for i in 0..vl {
-//         assert_eq!(out[i], inp1[i] + inp2[i]);
-//     }
-// }
-
-fn main() {
-}
+fn main() {}
