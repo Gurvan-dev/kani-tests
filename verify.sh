7446caff138371fd07c73f410f297c34ae82772a
Gurvan Debaussart <gurvan@debauss.art>
2026-08-17 17:07:46 +0200

    improve verification script

--- verify.sh
+++ verify.sh
@@ -1,8 +1,39 @@
 #!/bin/sh
 
+set -e
+
+# This is hardcoded for now but could be configured
 PATH="/home/gurvan/git/uni/phd/verif-riscv/kani-dev/scripts:${PATH}"
-# Important options:
-# --exact: Just do the specified harness
-# --only-codegen
-# --output-format terse
-RUSTFLAGS="--emit mir" cargo kani -Z const-prop-prune --harness=check_memset2
+
+# NOTE(Useful kani options):
+# --exact                       Just do the specified harness
+# --only-codegen                It's in the name
+# --output-format terse         Print only useful generation
+
+show_help() {
+    echo "Usage: ${0} [target] [harness]"
+    echo "Targets:"
+    echo "    smt         Dump cbmc smt queries"
+    echo "    check       Check options"
+}
+
+TARGET="${1}"
+KANI_OPT="-Z unstable-options -Z const-prop-prune"
+if [ -n "${2}" ]; then
+    KANI_OPT="${KANI_OPT} --harness=${2}"
+fi
+
+case "${TARGET}" in
+    check)
+        RUSTFLAGS="--emit mir" cargo kani ${KANI_OPT} --harness=kani_minimal_memset2
+        ;;
+
+    smt)
+        cargo kani ${KANI_OPT} --output-format old --cbmc-args --smt2 --outfile query.smt2
+        ;;
+    *)
+        echo "Error: Unknown target '${TARGET}'" >&2
+        show_help
+        exit 1
+        ;;
+esac
