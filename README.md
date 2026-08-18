c8784f7902711dc35a9a7b1f33b79e005b8a4376
Gurvan Debaussart <gurvan@debauss.art>
2026-08-18 16:37:02 +0200

    add kani as a submodule

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+
 cargo kani --output-format terse --harness check_vec_add_second_partial
+
+```
+git submodule update --init --recursive
+cd kani-dev && cargo build-dev
+```
--- verify.sh
+++ verify.sh
@@ -2,38 +2,47 @@
 
 set -e
 
-# This is hardcoded for now but could be configured
-# PATH="/home/gurvan/git/uni/phd/verif-riscv/kani-dev/scripts:${PATH}"
-
-# NOTE(Useful kani options):
-# --exact                       Just do the specified harness
-# --only-codegen                It's in the name
-# --output-format terse         Print only useful generation
+BASE_DIR="$(realpath "$(dirname "${0}")")"
+PATH="${BASE_DIR}/kani-dev/scripts:${PATH}"
 
 show_help() {
-    echo "Usage: ${0} [target] [harness]"
+    echo "Usage: ${0} <target> <harness1> [harness2 ...]"
     echo "Targets:"
     echo "    smt         Dump cbmc smt queries"
     echo "    check       Check options"
 }
 
-TARGET="${1}"
-# KANI_OPT="-Z unstable-options -Z const-prop-prune"
-if [ -n "${2}" ]; then
-    KANI_OPT="${KANI_OPT} --harness=${2}"
+if [ "$#" -lt 2 ]; then
+    show_help
+    exit 1
 fi
 
-case "${TARGET}" in
-    check)
-        RUSTFLAGS="--emit mir" cargo kani ${KANI_OPT}
-        ;;
+TARGET="${1}"
+shift
 
-    smt)
-        cargo kani ${KANI_OPT} --output-format old --cbmc-args --smt2 --outfile query.smt2
-        ;;
-    *)
-        echo "Error: Unknown target '${TARGET}'" >&2
-        show_help
-        exit 1
-        ;;
-esac
+for HARNESS in "$@"; do
+    echo "# Running harness '${HARNESS}'"
+    case "${TARGET}" in
+        check)
+            TIME_FILE="${HARNESS}.time"
+            LOG_FILE="${HARNESS}.log"
+
+            command time -p sh -c \
+                "( RUSTFLAGS=\"--emit mir\" cargo kani ${KANI_OPT} --harness \"${HARNESS}\" ) > \"${LOG_FILE}\" 2>&1" \
+                2> "${TIME_FILE}"
+
+            echo "# Output saved to ${LOG_FILE}"
+            echo "# Timing saved to ${TIME_FILE}"
+            ;;
+
+        smt)
+            OUTFILE="${HARNESS}.smt2"
+            cargo kani ${KANI_OPT} --harness "${HARNESS}" --output-format old --cbmc-args --smt2 --outfile "${OUTFILE}"
+            ;;
+        *)
+            echo "Error: Unknown target '${TARGET}'" >&2
+            show_help
+            exit 1
+            ;;
+    esac
+done
